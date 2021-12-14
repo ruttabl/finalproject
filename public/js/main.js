20a89cbd205e7927bdd8975e4d4215ba8bbfd980
Devin <devintaylor43@yahoo.com>
2021-12-14 01:46:29 -0500

    lol

--- public/js/main.js
+++ public/js/main.js
@@ -3,13 +3,18 @@ var ABTN = document.getElementById("abutton");
 var gcounter = localStorage.getItem("gpuamnt");
 var scaler = localStorage.getItem("uminer");
 var auto = localStorage.getItem("aminer");
-document.getElementById("gpuamnt").innerHTML = gcounter;
 BTN.addEventListener("click", addNum);
 var shopamount = localStorage.getItem("shopamnt");
 var ucounter = document.getElementById("amnt").innerHTML = shopamount;
 function addNum() { 
-    var counter = document.getElementById("amnt").innerHTML
-    var gum = Number(counter) + 1 * gcounter;
+    var counter = parseInt(document.getElementById("amnt").innerHTML)
+    if (gcounter > 0){
+        document.getElementById("gpuamnt").innerHTML = gcounter;
+        var gum = parseInt(counter) + 1 * gcounter;
+    }
+    else {
+        var gum = parseInt(counter) + 1;
+    }
     document.getElementById("amnt").innerHTML = gum;
     localStorage.setItem("samnt", Number(document.getElementById("amnt").innerHTML));
 }
@@ -19,39 +24,39 @@ homie.addEventListener("click", saveNum);
         localStorage.setItem("samnt", Number(document.getElementById("amnt").innerHTML));
     }
     
-var button = document.getElementById("abutton");
-button.innerHTML = "Start Auto Clicker"
-button.addEventListener ("click", function() {
-     if(stop===0) {
-    stop = 1
-    button.innerHTML = "Start Auto Clicker"
-    } else if(stop===1) {
-    stop = 0
-    button.innerHTML = "Stop Auto Clicker"
-    }
-    });
-var bttn = document.getElementById("gbutton");
-var stop = 1;
-function click() {
-    if(stop===0) {
-    bttn.click();
-}
-};
-setInterval("click()", Number(scaler));
-BTN.addEventListener("click", autoCheck);
-function autoCheck(){
-    if(auto = 0){
-        BTN.style.display = "block";
-        ABTN.style.display = "none";
-    }
-    else{
-        ABTN.style.display = "block";
-        BTN.style.display = "none";
-    }
-}
-window.addEventListener("onbeforeunload", save());
-function save() {
-    localStorage.setItem("samnt", Number(document.getElementById("amnt").innerHTML));
-}
+// var button = document.getElementById("abutton");
+// button.innerHTML = "Start Auto Clicker"
+// button.addEventListener ("click", function() {
+//      if(stop===0) {
+//     stop = 1
+//     button.innerHTML = "Start Auto Clicker"
+//     } else if(stop===1) {
+//     stop = 0
+//     button.innerHTML = "Stop Auto Clicker"
+//     }
+//     });
+// var bttn = document.getElementById("gbutton");
+// var stop = 1;
+// function click() {
+//     if(stop===0) {
+//     bttn.click();
+// }
+// };
+// setInterval("click()", Number(scaler));
+// BTN.addEventListener("click", autoCheck);
+// function autoCheck(){
+//     if(auto = 0){
+//         BTN.style.display = "block";
+//         ABTN.style.display = "none";
+//     }
+//     else{
+//         ABTN.style.display = "block";
+//         BTN.style.display = "none";
+//     }
+// }
+// window.addEventListener("onbeforeunload", save());
+// function save() {
+//     localStorage.setItem("samnt", Number(document.getElementById("amnt").innerHTML));
+// }
 
 
