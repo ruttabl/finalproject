5ff18a8f77eb32f5b30b6e9db3ddc1af748aff95
Devin <devintaylor43@yahoo.com>
2021-12-14 02:11:08 -0500

    almost

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,5 @@
 var BTN = document.getElementById("gbutton");
 var ABTN = document.getElementById("abutton");
-var gcounter = localStorage.getItem("gpuamnt");
 var scaler = localStorage.getItem("uminer");
 var auto = localStorage.getItem("aminer");
 BTN.addEventListener("click", addNum);
@@ -9,7 +8,7 @@ var ucounter = document.getElementById("amnt").innerHTML = shopamount;
 function addNum() { 
     var counter = parseInt(document.getElementById("amnt").innerHTML)
     if (gcounter > 0){
-        document.getElementById("gpuamnt").innerHTML = gcounter;
+        var gcounter = localStorage.getItem("gpuamnt");
         var gum = parseInt(counter) + 1 * gcounter;
     }
     else {
@@ -24,25 +23,25 @@ homie.addEventListener("click", saveNum);
         localStorage.setItem("samnt", Number(document.getElementById("amnt").innerHTML));
     }
     
-// var button = document.getElementById("abutton");
-// button.innerHTML = "Start Auto Clicker"
-// button.addEventListener ("click", function() {
-//      if(stop===0) {
-//     stop = 1
-//     button.innerHTML = "Start Auto Clicker"
-//     } else if(stop===1) {
-//     stop = 0
-//     button.innerHTML = "Stop Auto Clicker"
-//     }
-//     });
-// var bttn = document.getElementById("gbutton");
-// var stop = 1;
-// function click() {
-//     if(stop===0) {
-//     bttn.click();
-// }
-// };
-// setInterval("click()", Number(scaler));
+var button = document.getElementById("abutton");
+button.innerHTML = "Auto Mine!"
+button.addEventListener ("click", function() {
+     if(stop===0) {
+    stop = 1
+    button.innerHTML = "Auto Mine!"
+    } else if(stop===1) {
+    stop = 0
+    button.innerHTML = "Pause Auto Mine"
+    }
+    });
+var bttn = document.getElementById("gbutton");
+var stop = 1;
+function click() {
+    if(stop===0) {
+    bttn.click();
+}
+};
+setInterval("click()", 200);
 // BTN.addEventListener("click", autoCheck);
 // function autoCheck(){
 //     if(auto = 0){
